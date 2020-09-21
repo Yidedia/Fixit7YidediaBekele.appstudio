4835ab2f418bc35c5daaa375ea8b2b0b6e60fdeb
Yidedia <70745425+Yidedia@users.noreply.github.com>
2020-09-20 19:32:21 -0500

    Script last form

---
+++ forms/strings/strings.js
@@ -0,0 +1,19 @@
+//var A
+let quoteString = "Failure is simply the opportunity to begin again this time more intelligently."
+//cap B
+let upperCaseString = quoteString.toUpperCase()
+console.log(`Upper case string is: ${upperCaseString}`)
+//author var C
+let authorString = "- Henry Ford"
+//combine D
+let completeString = quoteString + authorString
+console.log(`The string in completeString is: ${completeString}`)
+// var E
+let secondQuote = "Talent is cheaper than table salt. What separates the talented individual from the successful one is a lot of hard work. - Stephen King"
+// low F
+let lowerCaseString = quoteString.toLowerCase()
+console.log(`Lower case string is: ${lowerCaseString}`)
+// g
+let num3 = Number(prompt('Enter a number to be added by 10'))
+let sumNum = addinput(num3,10)
+alert(`The number ${num3} + 10 is ${sumNum}.`)
